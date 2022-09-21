4cdaa1fa1dc5c9c8c65dd1a55386c6ada3ba421f
unknown <Cedran-Bastien>
2022-09-21 15:41:35 +0200

    Test Etudiant

--- Test/TestEtudiant.java
+++ Test/TestEtudiant.java
@@ -1,5 +1,13 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledForJreRange;
+
+import java.text.Normalizer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TestEtudiant {
     Etudiant etudiant;
@@ -7,6 +15,32 @@ public class TestEtudiant {
 
     @BeforeEach
     public void preparationTest(){
-        
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put("Anglais", 2);
+        map.put("Francais",1);
+
+        Formation form = new Formation(254, map);
+
+        Identite id = new Identite("BASTIEN", "Cedran", "125455225");
+
+        Map<String, List<Integer>> result = new HashMap<String, List<Integer>>();
+        List<Integer> note =new ArrayList<Integer>();
+        result.put("Anglais",note);
+        List<Integer> noteF =new ArrayList<Integer>();
+        result.put("Francais",noteF);
+
+        etudiant = new Etudiant(id,form,result);
     }
+
+    @Test
+    public void Test_ajout(){
+        etudiant.ajouterNote("Francais",15);
+        etudiant.ajouterNote("Francais",16);
+        Assertions.assertEquals(15,etudiant.getResultat().get("Francais").get(0));
+        Assertions.assertEquals(16,etudiant.getResultat().get("Francais").get(1));
+    }
+
+
+    @Test
+    public void Test_CalculeMoyenneGeneral()
 }
