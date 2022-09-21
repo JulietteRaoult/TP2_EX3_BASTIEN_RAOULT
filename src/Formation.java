444ddf4a77f076e64256b3ac62dea77922edc10e
unknown <Cedran-Bastien>
2022-09-21 14:44:36 +0200

    classe Formation

---
+++ src/Formation.java
@@ -0,0 +1,29 @@
+import com.sun.source.tree.LineMap;
+import jdk.jshell.execution.StreamingExecutionControl;
+
+import java.util.Map;
+
+public class Formation {
+    private int id;
+    private Map<String, Integer> matiere;
+
+    public Formation(int id, Map<String, Integer> mat){
+        this.id = id;
+        this.matiere = mat;
+    }
+
+    public void supprimer(String matiere){
+        this.matiere.remove(matiere);
+    }
+
+    public void ajouter(String matiere, int coef){
+        this.matiere.put(matiere,coef);
+    }
+
+    public void getCoef(String matiere){
+        
+        this.matiere.get(matiere);
+    }
+
+
+}
